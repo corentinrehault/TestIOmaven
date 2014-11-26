b566c00807fa2e8c49f5300f1262c49f47ea48a0
corentinrehault <corentin.rehault@gmail.com>
2014-11-26 16:45:41 +0100

    Envoi TestIOmaven

---
+++ TestIOmaven/src/main/java/fr/macie/TesIOmaven/App.java
@@ -0,0 +1,13 @@
+package fr.macie.TesIOmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
